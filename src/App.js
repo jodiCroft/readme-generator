5e31894fd2c98d1df082c17f984488033f6f96a9
Jodi Croft <jodicroft@jodis-mbp.lan>
2021-01-28 16:58:10 -0600

    all styling done.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Markdown from "./Components/Markdown";
 import Preview from "./Components/Preview";
 
 function App() {
-  let textAreaHeight = 50;
   const [text, setText] = useState(`# Foobar
 
   Foobar is a Python library for dealing with word pluralization.
@@ -14,17 +13,31 @@ function App() {
   
   Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
   
-  \`\`\`
+  \`\`\` bash
   pip install foobar
   \`\`\`
+  
+  ## Usage
+  
+  \`\`\` python
+  import foobar
+  
+  foobar.pluralize('word') # returns 'words'
+  foobar.pluralize('goose') # returns 'geese'
+  foobar.singularize('phenomena') # returns 'phenomenon'
+  \`\`\`
+  
+  ## Contributing
+  Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+  
+  Please make sure to update tests as appropriate.
+  
+  ## License
+  [MIT](https://choosealicense.com/licenses/mit/)
   `);
 
   const handleChange = (e) => {
     setText(e.target.value);
-    let maxHeight = Infinity;
-    let textAreaRows = e.target.value.split("\n");
-    console.log(textAreaRows.length);
-    // if(textAreaRows != "undefined" && textAreaRows.length < maxHeight)
   };
 
   return (
