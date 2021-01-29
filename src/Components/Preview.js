2b71794ba7e097084a77a45aa5be61241cafe960
Jodi Croft <jodicroft@jodis-mbp.lan>
2021-01-29 14:32:35 -0600

    fixed code snippets overflowing.

--- src/Components/Preview.js
+++ src/Components/Preview.js
@@ -4,11 +4,13 @@ import ReactMarkdown from "react-markdown";
 const Preview = ({ text }) => {
   return (
     <div className="TemplateArea">
-      <div className="TitleWrapper">
-        <h3 className="PreviewTitle">See preview here</h3>
-      </div>
-      <div className="PreviewOutput">
-        <ReactMarkdown>{text}</ReactMarkdown>
+      <div className="PreviewWrapper">
+        <div className="TitleWrapper">
+          <h3 className="PreviewTitle">See preview here</h3>
+        </div>
+        <div className="PreviewOutput">
+          <ReactMarkdown className="Code">{text}</ReactMarkdown>
+        </div>
       </div>
     </div>
   );
